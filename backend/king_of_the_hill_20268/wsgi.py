3499a1496c2b89e53f8419d3cf0e40df58658ed4
Crowdbotics <team@crowdbotics.com>
2020-09-14 22:22:42 +0000

    Update models

--- backend/king_of_the_hill_20268/wsgi.py
+++ backend/king_of_the_hill_20268/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'king_of_the_hill_20268.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "king_of_the_hill_20268.settings")
 
 application = get_wsgi_application()
